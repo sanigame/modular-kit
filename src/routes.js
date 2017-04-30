059366a0badc53b6f0488c45b7106e6c71cb553e
Supasan Suklim <supasan.game@gmail.com>
2017-04-30 23:50:12 +0700

    Fix scene array

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
 import { project } from './config';
-import * as feature from './features'
 import * as scene from './scenes'
 
 export const Scenes = [
-  { key: 'modularHome', component: 'scene.Home', title: '', options: {} },
-  { key: 'modularAbout', component: 'scene.About', title: '', options: {} },
+  { key: 'modularAbout', component: 'About', type: 'scene', title: 'm About', options: {} },
 ]
 
 const renderScenes = (sceneArr = []) => {
-  return sceneArr.map((scene, index) => {
-    return(<Scene key={scene.key} component={About} title={scene.title}/>)
+  return sceneArr.map((value, index) => {
+    return(<Scene key={value.key} component={scene[value.component]} title={value.title}/>)
   })
 }
 
 const Routes = () => {
   return (
     <Router sceneStyle={{ marginTop: 70 }}>
+      <Scene key="modularHome" component={scene.Home} title="m Home" initial />
       {renderScenes(Scenes)}
     </Router>
   )
