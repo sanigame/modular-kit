a50bbd503a53fc7ca93488369cef983c684527a7
Supasan Suklim <supasan.s@dotography.com>
2017-05-03 14:01:40 +0700

    Export all form scene

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 export { Feed, FeedContainer, reducers } from './features'
 
 // export scenes
-export { Home, About } from './scenes'
+export { Home, About, Form, DefaultFrom, InitFrom, ValidateFrom } from './scenes'
 
 // export router scene
 export { Scenes } from './routes'
